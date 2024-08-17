89321db9801209637edb2288535aa60bf6ef3dc1
Koulagaribhargavi <2311cs010360@mallareddyuniversity.ac.in>
2024-08-17 09:05:26 +0530

    Add files via upload

---
+++ Bubblesort.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bubblesort;
+import java.util.Arrays;
+public class Bubblesort {
+ public void bubbleSort(int[] intArray) {
+ for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0;
+ lastUnsortedIndex--) {
+ for (int i = 0; i < lastUnsortedIndex; i++) {
+ if (intArray[i] > intArray[i + 1]) {
+ swap(intArray, i, i + 1);
+ }
+ }
+ }
+ }
+ public static void swap(int[] array, int i, int j) {
+ if (i == j) {
+ return;
+ }
+ int temp = array[i];
+ array[i] = array[j];
+ array[j] = temp;
+ }
+ public static void main(String[] args) {
+ Bubblesort bs = new Bubblesort();
+ int array[] = {5, 1, 2, 4, 3};
+ System.out.println(Arrays.toString(array));
+ bs.bubbleSort(array);
+ System.out.println(Arrays.toString(array));
+ }
+}
